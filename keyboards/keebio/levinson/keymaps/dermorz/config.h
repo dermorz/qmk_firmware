a8666696bff03078c7f37e7a2473112723341963
Moritz Bracht <mail@morz.me>
2022-07-11 09:13:46 +0200

    Add CAPS_WORD_ENABLE on double tap shift

--- keyboards/keebio/levinson/keymaps/dermorz/config.h
+++ keyboards/keebio/levinson/keymaps/dermorz/config.h
@@ -51,3 +51,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_ACCELERATED_SPEED 400
 #define MOUSEKEY_MAX_SPEED 3
 #define MOUSEKEY_TIME_TO_MAX 50
+
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
